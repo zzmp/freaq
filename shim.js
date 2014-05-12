5fd7fe0743f689ec0a599fa656d9665e64d885c1
zzmp <zmp@umich.edu>
2014-05-11 17:15:29 -0700

    Updated shim.

--- shim.js
+++ shim.js
@@ -1,37 +1,39 @@
 define(function () {
   return {
-    AudioContext: window.AudioContext                         || 
-                  window.webkitAudioContext                   ||
-                  window.mozAudioContext                      || 
-                  window.oAudioContext                        ||
-                  window.msAudioContext,
-    requestAnimationFrame: window.requestAnimationFrame.bind(window)       || 
-                           window.webkitRequestAnimationFrame.bind(window) || 
-                           window.mozRequestAnimationFrame.bind(window)    || 
-                           window.oRequestAnimationFrame.bind(window)      || 
-                           window.msRequestAnimationFrame.bind(window)     ||
-                           function (callback) {
-                             window.setTimeout(callback, 1000 / 17);
-                           },
-    requestFullscreen: window.requestFullscreen               ||
-                       window.webkitRequestFullScreen         ||
-                       window.mozRequestFullScreen            ||
-                       window.oRequestFullScreen              ||
-                       window.msRequestFullScreen,
-    fullscreenElement: document.FullscreenElement             ||
-                       document.webkitFullscreenElement       ||
-                       document.webkitFullScreenElement       ||
-                       document.mozFullscreenElement          ||
-                       document.mozFullScreenElement          ||
-                       document.oFullscreenElement            ||
-                       document.oFullScreenElement            ||
-                       document.msFullscreenElement           ||
-                       document.msFullScreenElement,
+    AudioContext: window.AudioContext ||
+                  window.webkitAudioContext,
+    requestAnimationFrame: function(render) {
+      var f = window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame;
+      return f ? f.call(window, render) : function (callback) {
+        window.setTimeout(callback, 1000 / 24);
+      };
+    },
+    requestFullscreen: function() {
+      var requestFullscreen = this.requestFullscreen       ||
+                              this.webkitRequestFullScreen ||
+                              this.mozRequestFullScreen    ||
+                              this.msRequestFullScreen;
+      requestFullscreen.call(this);
+    },
+    exitFullscreen: function() {
+      var exitFullscreen = document.exitFullscreen       ||
+                           document.webkitExitFullScreen ||
+                           document.mozCancelFullScreen  ||
+                           document.msExitFullScreen;
+      exitFullscreen.call(document);
+    },
+    fullscreenElement: function() {
+      return document.FullscreenElement       ||
+             document.webkitFullScreenElement ||
+             document.mozFullScreenElement    ||
+             document.msFullScreenElement;
+    },
     onfullscreenchange: function (callback, useCapture) {
       this.addEventListener('fullscreenchange', callback, useCapture);
       this.addEventListener('webkitfullscreenchange', callback, useCapture);
       this.addEventListener('mozfullscreenchange', callback, useCapture);
-      this.addEventListener('ofullscreenchange', callback, useCapture);
       this.addEventListener('msfullscreenchange', callback, useCapture);
     }
   };
